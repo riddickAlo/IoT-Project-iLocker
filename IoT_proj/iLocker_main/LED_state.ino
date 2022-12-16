2714588c2ed8e80eced15e92e90e2a370fbf2d0d
riddickAlo <103126128+riddickAlo@users.noreply.github.com>
2022-12-16 10:57:21 +0800

    Update LED_state.ino

--- IoT_proj/iLocker_main/LED_state.ino
+++ IoT_proj/iLocker_main/LED_state.ino
@@ -1,17 +1,17 @@
  /* Control LED state */
 void LED_state(unsigned long tstamp) {
-  if(lock_state){
+  if(lock_state){           // locked, RL on
     digitalWrite(RL_pin, 1);
     digitalWrite(YL_pin, 0);
     digitalWrite(GL_pin, 0);
   }
-  else {
+  else {                    // being reserved, YL on
     digitalWrite(RL_pin, 0);
     if(strlen(&current_user[0]) > 0) {
       digitalWrite(YL_pin, 1);
       digitalWrite(GL_pin, 0);
     }
-    else {
+    else {                  // unlocked & available, GL on
       digitalWrite(YL_pin, 0);
       digitalWrite(GL_pin, 1);
     }
