3d3a18d8ac764b1b61931fc6c495e6ed1ef96a42
riddickAlo <103126128+riddickAlo@users.noreply.github.com>
2022-12-16 10:54:34 +0800

    Add files via upload

---
+++ IoT_proj/iLocker_cam/iLocker_cam.ino
@@ -0,0 +1,279 @@
+/* For esp32 cam module */ 
+#include <Arduino.h>
+#include <WiFi.h>             // for esp 32 cam
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+#include <WiFiUdp.h>
+#include <string.h>
+
+//const char* ssid = "Factory2_2.4G";
+//const char* password = "118factory2";
+const char* ssid = "EEB02-2(2.4G)";
+const char* password = "IOTEEB0202";
+
+//-----------------------------------------------------------------------------------------------------------
+WiFiUDP Udp;                      //Create a UDP class
+unsigned int localUdpPort = 3000;
+char incomingPacket[20];
+
+//-----------------------------------------------------------------------------------------------------------
+
+// Initialize Telegram BOT
+//String BOTtoken = "5539838452:AAF1JQ5_fuAp3M7D9QnMkTElcCvUSpYpIVs";  // your Bot Token (Get from Botfather)
+String BOTtoken = "5550726322:AAEjE7CNkNmunGwwJEFnmjDC6Z2nXGoztps"; //
+//String CHAT_ID = "5593525882";
+//String CHAT_ID = "840689318";
+String CHAT_ID = "";
+
+bool sendPhoto = false;
+
+WiFiClientSecure clientTCP;
+UniversalTelegramBot bot(BOTtoken, clientTCP);
+
+#define FLASH_LED_PIN 4
+bool is_InCam =true;              // if it's inside camera, set to true
+
+//Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+int LightValue1;
+int LightValue2;
+void configInitCamera(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+
+  // Drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_CIF);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+}
+
+
+String sendPhotoTelegram() {
+  const char* myDomain = "api.telegram.org";
+  String getAll = "";
+  String getBody = "";
+
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  esp_camera_fb_return(fb);
+  fb = NULL;
+  fb = esp_camera_fb_get();
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+    return "Camera capture failed";
+  }  
+  
+  Serial.println("Connect to " + String(myDomain));
+
+  if (clientTCP.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
+    
+    String head = "--Electro\r\nContent-Disposition: form-data; name=\"chat_id\"; \r\n\r\n" + CHAT_ID + "\r\n--Electro\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--Electro--\r\n";
+
+    uint16_t imageLen = fb->len;
+    uint16_t extraLen = head.length() + tail.length();
+    uint16_t totalLen = imageLen + extraLen;
+  
+    clientTCP.println("POST /bot"+BOTtoken+"/sendPhoto HTTP/1.1");
+    clientTCP.println("Host: " + String(myDomain));
+    clientTCP.println("Content-Length: " + String(totalLen));
+    clientTCP.println("Content-Type: multipart/form-data; boundary=Electro");
+    clientTCP.println();
+    clientTCP.print(head);
+  
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n=0;n<fbLen;n=n+1024) {
+      if (n+1024<fbLen) {
+        clientTCP.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen%1024>0) {
+        size_t remainder = fbLen%1024;
+        clientTCP.write(fbBuf, remainder);
+      }
+    }  
+    
+    clientTCP.print(tail);
+    
+    esp_camera_fb_return(fb);
+    
+    int waitTime = 10000;   // timeout 10 seconds
+    long startTimer = millis();
+    boolean state = false;
+    
+    while ((startTimer + waitTime) > millis()){
+      Serial.print(".");
+      delay(100);      
+      while (clientTCP.available()) {
+        char c = clientTCP.read();
+        if (state==true) getBody += String(c);        
+        if (c == '\n') {
+          if (getAll.length()==0) state=true; 
+          getAll = "";
+        } 
+        else if (c != '\r')
+          getAll += String(c);
+        startTimer = millis();
+      }
+      if (getBody.length()>0) break;
+    }
+    clientTCP.stop();
+    Serial.println(getBody);
+  }
+  else {
+    getBody="Connected to api.telegram.org failed.";
+    Serial.println("Connected to api.telegram.org failed.");
+  }
+  return getBody;
+}
+
+void setup(){
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  // Set LED Flash as output
+  pinMode(FLASH_LED_PIN, OUTPUT);
+  digitalWrite(FLASH_LED_PIN, 0);
+
+  // Config and init the camera
+  configInitCamera();
+
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("ESP32-CAM IP Address: ");
+  Serial.println(WiFi.localIP()); 
+  //------------------------------------------------------------------------------------------
+  Udp.begin(localUdpPort);
+  //------------------------------------------------------------------------------------------
+
+}
+
+void loop() {
+
+
+  LightValue1 = digitalRead(2); //讀取的GPIO 13讀取的數值放在LightValue
+  LightValue2 = digitalRead(4);
+  //Serial.println(LightValue1);
+  //Serial.println(LightValue2);
+  
+  if (sendPhoto) {
+    if(is_InCam)
+      digitalWrite(FLASH_LED_PIN, 1);
+      
+    Serial.println("Preparing photo");
+    sendPhotoTelegram(); 
+    Serial.println(CHAT_ID);
+    sendPhoto = false;
+    if(is_InCam)
+      digitalWrite(FLASH_LED_PIN, 0);
+  }
+
+
+  if(Udp.parsePacket()){
+    //int len = 
+    Udp.read(incomingPacket, 20);   
+    if(incomingPacket[0] == 'c'){
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      // Just test touch pin - Touch0 is T0 which is on GPIO 4.
+      Udp.printf("ACK0");
+      Udp.endPacket();
+    }
+    else if(incomingPacket[0] == 'u'){
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      // Just test touch pin - Touch0 is T0 which is on GPIO 4.
+      Udp.printf("ACK1");
+      Udp.endPacket();
+      uint8_t i=0;   
+      CHAT_ID = "";
+      while(incomingPacket[i+2]!='/'){
+        CHAT_ID += incomingPacket[i+2];
+        i++;
+        }
+      Serial.printf("id: %s \n", CHAT_ID);      
+    }
+     else if(incomingPacket[0] == 's'){
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      // Just test touch pin - Touch0 is T0 which is on GPIO 4.
+      Udp.printf("ACK2");
+      Udp.endPacket();
+      sendPhoto = true;
+    }
+  }
+}
